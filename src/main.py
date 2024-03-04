4e5750e27e6b336e20ca998f429e0f2036729442
cl0udy <agenteyt11@gmail.com>
2024-03-04 08:19:54 -0500

    Initital Commit, boilerplate code

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       ecweyer                                                      #
+# 	Created:      3/4/2024, 8:13:56 AM                                         #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
